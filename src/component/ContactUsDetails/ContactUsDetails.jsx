79928a30016ecefd415c48017adc3edcb0046815
Husnain imran <53654414+husnain1243@users.noreply.github.com>
2024-03-25 00:08:36 +0500

    first commit

--- src/component/ContactUsDetails/ContactUsDetails.jsx
+++ src/component/ContactUsDetails/ContactUsDetails.jsx
@@ -1,21 +1,21 @@
 export const ContactUsDetails = () => {
     return(
         <div className="row gap-3 d-flex flex-wrap justify-content-center">
-            <div className="col-11 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
+            <div className="col-5 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#e8bd81dc" className="bi bi-geo-alt-fill mb-3" viewBox="0 0 16 16">
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                 </svg>
                 <h3>OUR MAIN OFFICE</h3>
                 <p><a href="">Lahore,Pakistan</a></p>
             </div>
-            <div className="col-11 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
+            <div className="col-5 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#e8bd81dc" className="bi bi-telephone-forward-fill mb-3" viewBox="0 0 16 16">
                     <path fillule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"/>
                 </svg>
                 <h3 className="mb-2">PHONE NUMBER</h3>
                 <p><a href="tel:+923164604121">+923164604121</a></p>
             </div>
-            <div className="col-11 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
+            <div className="col-5 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#e8bd81dc" className="bi bi-printer-fill mb-3" viewBox="0 0 16 16">
                     <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
                     <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
@@ -23,7 +23,7 @@ export const ContactUsDetails = () => {
                 <h3>LANDLINE</h3>
                 <p><a href="tel:04237132996">04237132996</a></p>
             </div>
-            <div className="col-11 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
+            <div className="col-5 col-md-5 text-center d-flex align-items-center py-5 px-2 card gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#e8bd81dc" className="bi bi-envelope-fill mb-3" viewBox="0 0 16 16">
                     <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                 </svg>
